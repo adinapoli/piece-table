9ebad199d1a65bd89a9c4aaacb76dbff3a45857a
Alfredo Di Napoli <alfredo.dinapoli@gmail.com>
2016-07-13 08:20:58 +0200

    Stubbed the API, and wrote 2 (failing) tests

--- src/Data/Text/PieceTable.hs
+++ src/Data/Text/PieceTable.hs
@@ -23,6 +23,16 @@ module Data.Text.PieceTable (
   , PieceTable
   , new
   , newFromText
+  -- * API as described in the paper
+  , empty
+  , insert
+  , delete
+  , itemAt
+  -- * Operation on ranges
+  , insertSequence
+  , deleteSequence
+  , sequenceAt
+  , replace
   -- * Unsafe functions
   , unsafeRender
   ) where
@@ -44,7 +54,7 @@ data FileType = Original
 data Piece = Piece {
     fileType :: FileType
     -- ^ Which buffer the `Piece` refers to.
-  , start  :: !Int
+  , start  :: !Position
     -- ^ On offset into that buffer.
   , length :: !Int
     -- ^ The length of the piece.
@@ -73,6 +83,16 @@ unsafeRender PieceTable{..} = case table of
         Original -> acc <> render fileBuffer
         Buffer   -> acc <> render addBuffer
 
+{- | We want to support the API as described in the paper:
+
+typedef Position = int; //a position in the sequence, starts from 0
+typedef Item unsigned char;
+
+-}
+
+type Position = Int
+type Item = Char
+
 --------------------------------------------------------------------------------
 new :: FilePath -> IO PieceTable
 new fp = do
@@ -90,3 +110,49 @@ new' c = PieceTable {
 --------------------------------------------------------------------------------
 newFromText :: T.Text -> PieceTable
 newFromText = new'
+
+--------------------------------------------------------------------------------
+empty :: PieceTable
+empty = PieceTable {
+    table = mempty
+    , fileBuffer = T.empty
+    , addBuffer  = T.empty
+    }
+
+--------------------------------------------------------------------------------
+-- | Inserts a single `Item` at `Position` in the given `PieceTable`.
+insert :: Item -> Position -> PieceTable -> PieceTable
+insert _ _ p = p
+
+--------------------------------------------------------------------------------
+insertSequence :: T.Text -> Position -> PieceTable -> PieceTable
+insertSequence _ _ p = p
+
+--------------------------------------------------------------------------------
+-- | Deletes a single `Item` at `Position` in the given `PieceTable`.
+delete :: Position -> PieceTable -> PieceTable
+delete _ p = p
+
+--------------------------------------------------------------------------------
+deleteSequence :: (Position, Position)
+               -- ^ (start, end)
+               -> PieceTable
+               -- ^ The original `PieceTable`
+               -> PieceTable
+deleteSequence _ p = p
+
+--------------------------------------------------------------------------------
+itemAt :: Position -> PieceTable -> Maybe Item
+itemAt _ _ = Nothing
+
+--------------------------------------------------------------------------------
+sequenceAt :: Position -> PieceTable -> T.Text
+sequenceAt _ _ = T.empty
+
+--------------------------------------------------------------------------------
+replace :: Position
+        -- ^ start
+        -> T.Text
+        -> PieceTable
+        -> PieceTable
+replace _ _ p = p
--- test-suite/Main.hs
+++ test-suite/Main.hs
@@ -14,4 +14,8 @@ main = do
 spec :: Spec
 spec = parallel $ do
     it "unsafeRender of an unsplitted text works" $ do
-        unsafeRender (newFromText "hello with spaces") == "hello with spaces"
+        unsafeRender (newFromText "hello with spaces") `shouldBe` "hello with spaces"
+    it "Adding a single char works" $ do
+        unsafeRender (insert 'a' 0 $ newFromText "hello with spaces") `shouldBe` "ahello with spaces"
+    it "Deleting a single char works" $ do
+        unsafeRender (delete 0 $ newFromText "hello with spaces") `shouldBe` "ello with spaces"
